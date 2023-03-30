4a5c5d63e7cc0469640c4bb23c32da2ed19a126d
developer-nieem <naimhossenwork@gmail.com>
2023-03-30 22:18:30 +0600

    card design

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     
+    <div className="App container">
+      <Header></Header>
+     <Home></Home>
     </div>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainData from './MainData/MainData';
+import Sidebar from './Sidebar/Sidebar';
+
+const Home = () => {
+    return (
+        <div>
+            <MainData></MainData>
+            <Sidebar></Sidebar>
+        </div>
+    );
+};
+
+export default Home;
