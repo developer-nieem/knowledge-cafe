99325805da974fc66e1e9dc4f586033d1fd8178b
developer-nieem <naimhossenwork@gmail.com>
2023-03-30 22:41:41 +0600

    fetch json data and load data on display

--- src/components/Home/MainData/MainData.jsx
+++ src/components/Home/MainData/MainData.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleBlog from '../SingleBlog/SingleBlog';
 
 const MainData = () => {
+
+    const [blogs, setBlogs] = useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
     return (
         <div>
-            <SingleBlog></SingleBlog>
+            {
+              blogs.map(post =><SingleBlog post={post}></SingleBlog>)  
+            }
+            
         </div>
     );
 };
--- src/components/Home/SingleBlog/SingleBlog.jsx
+++ src/components/Home/SingleBlog/SingleBlog.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-const SingleBlog = () => {
+const SingleBlog = ({post}) => {
+    console.log(post.tags);
     return (
         <div>
            <div className="card border-0" >
-            <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" className="card-img-top" alt="..."/>
+            <img src={post.coverImage} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <div className='d-flex justify-content-between'>
                     <div className='d-flex justify-content-between gap-2'>
                         <div>
-                            <img className='rounded-circle' style={{width:'60px', height:'60px'}} src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="...." />
+                            <img className='rounded-circle' style={{width:'60px', height:'60px'}} src={post.userPhoto} alt="...." />
                         </div>
                         <div>
-                            <h3>Mr. Raju</h3>
-                            <p>Mar 14 (4 Days ago)</p>
+                            <h3>{post.authorName}</h3>
+                            <p>{post.publishDate}</p>
                         </div>
                     </div>
                     <div>
-                        <p>05 min read <button>book</button></p>
+                        <p>{post.readTime} min read <button>book</button></p>
                     </div>
                 </div>
-                    <h2>title</h2>
+                    <h2 className='my-4'>{post.title}</h2>
+                    <p>{post.tags.map(tag=> <span className='me-2'>{tag}</span>)}</p>
                 <a href="#" className="link-underline-primary">Mark as read</a>
                 
             </div>
