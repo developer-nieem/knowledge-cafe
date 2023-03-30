616b7e11763f8440efab3092e10cfeae4c35fb7d
developer-nieem <naimhossenwork@gmail.com>
2023-03-30 22:21:43 +0600

    home page design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,13 @@ import Sidebar from './Sidebar/Sidebar';
 
 const Home = () => {
     return (
-        <div>
+        <div className='row'>
+            <div className='col-md-9'>
             <MainData></MainData>
-            <Sidebar></Sidebar>
+            </div>
+            <div className="col-md-3">
+            <Sidebar ></Sidebar>
+            </div>
         </div>
     );
 };
