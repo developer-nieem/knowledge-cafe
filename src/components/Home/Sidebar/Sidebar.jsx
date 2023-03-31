498fca676f6544e42f0a2923d2a3c55f8172e5ce
developer-nieem <naimhossenwork@gmail.com>
2023-03-31 08:05:47 +0600

    fixed blog bookemarked issue

--- src/components/Home/Sidebar/Sidebar.jsx
+++ src/components/Home/Sidebar/Sidebar.jsx
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { getBlogItemCart } from '../../../utilities/fakedb';
 
 const Sidebar = ({cart,readTime}) => {
+    const [cartTitle , setCartTitle] = useState([]);
+
     const [time, setTime] = useState(readTime);
 
-    const getBlogTitle = getBlogItemCart();
-    let blogTitle = [];
+    // bookmarked blog
+    useEffect(()=>{
+        const getBlogTitle = getBlogItemCart();
+         let blogTitle = [];
     for (const key in getBlogTitle) {
-        blogTitle.push(key)
-            
+        blogTitle.push(key);  
         }
+        setCartTitle(blogTitle);
+    },[cart])
   
+
+
         // read time 
         useEffect(()=>{
             const getItem = localStorage.getItem('readTime');
@@ -20,9 +27,9 @@ const Sidebar = ({cart,readTime}) => {
     return (
         <div >
             <h5 className='bg-white p-3 rounded my-4 text-primary'>Spent time on read : {time} min</h5>
-            <h5 className='fw-bold'>Bookmarked Blogs : {blogTitle.length}</h5>
+            <h5 className='fw-bold'>Bookmarked Blogs : {cartTitle.length}</h5>
             {
-                blogTitle.map(title => <h5 className='bg-white p-3 rounded'>{title}</h5>)
+                cartTitle.map(title => <h5 className='bg-white p-3 rounded'>{title}</h5>)
             }
         </div>
     );
